d2ce4e4db85ca4157c76d3299f23a56cc0546357
himara1027 <it21293030@my.sliit.com>
2025-07-13 06:17:35 +0530

    remove api genarate code

--- server/config/openaiConfig.js
+++
@@ -1,8 +0,0 @@
-// // server/config/openaiConfig.js
-// const OpenAI = require('openai');
-
-// const openai = new OpenAI({
-//   apiKey: process.env.OPENAI_API_KEY || 'your-hardcoded-key-here',
-// });
-
-// module.exports = openai;
--- server/routes/chatRoutes.js
+++
@@ -1,7 +0,0 @@
-// const express = require('express');
-// const { getCourseRecommendations } = require('../controllers/chatController');
-
-// const router = express.Router();
-// router.post('/recommend', getCourseRecommendations);
-
-// module.exports = router;
